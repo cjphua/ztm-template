9aaba0f988323fbfbe6f5ba63907ce84a073d728
CJ Phua <cjphuaa@gmail.com>
2022-03-29 23:25:45 +0800

    updated homepage with new title and slogan.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+  <div
+    class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center"
+  >
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Welcome to World Travel App</h1>
+      <p class="mt-5">Get the best travel itineary for your trip</p>
     </div>
   </div>
 </template>
