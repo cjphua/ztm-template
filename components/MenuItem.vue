c6180375d6218ffe9cef8e55908ac37ae81d6361
CJ Phua <cjphuaa@gmail.com>
2022-04-20 20:40:41 +0800

    email ready

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="text-center">
-    <div><img src="crispy-chicken.jpg" class="border border-gray p-5" alt="" /></div>
-    <h4 class="font-oswald uppercase text-2xl mt-5">Crispy Chicken</h4>
+    <div><img :src="'/' + item.image" class="border border-gray p-5" alt="" /></div>
+    <h4 class="font-oswald uppercase text-2xl mt-5">{{ item.name }}</h4>
     <p class="font-light text-gray-500 my-2">
-      Chicekn breast, Chili Sauce, Tomatoes, colesaw
+      {{ item.description }}
     </p>
     <p class="mt-5 mb-8">
       <span
         class="rounded rounded-lg bg-amber-800 text-yellow-400 font-oswald font-bold text-xl px-5 py-2"
-        >RM11.99</span
+        >RM{{ item.price.toFixed(2) }}</span
       >
     </p>
-    <button class="font-oswald uppercase border py-2 px-3">Add To Cart</button>
+    <button class="font-oswald uppercase border py-2 px-3" @click="addItem">
+      Add To Cart
+    </button>
   </div>
 </template>
 
 <script>
-export default { props: ['item'] };
+export default {
+  props: ['item'],
+  methods: {
+    addItem() {
+      this.$store.commit('addItem', this.item);
+    },
+  },
+};
 </script>
--- pages/menu.vue
+++ pages/menu.vue
@@ -6,11 +6,12 @@
           <img src="logo.png" class="w-150px" alt="" />
         </div>
         <div id="menu-right">
-          <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5">
+          <ul class="grid grid-cols-5 text-lg font-oswald text-white uppercase pt-5">
             <li>About Us</li>
-            <li>Our Menu</li>
+            <NuxtLink to="/menu">Our Menu</NuxtLink>
             <li>Location</li>
             <li>Contacts</li>
+            <NuxtLink to="/cart">cart ({{ $store.getters.totalItems }})</NuxtLink>
           </ul>
         </div>
       </div>
@@ -18,10 +19,20 @@
         Our menu
       </h1>
     </div>
-    <div id="item" class="grid grid-cols-3">
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
+    <div id="item" class="grid grid-cols-3 gap-x-5 gap-y-16 mt-10 mx-20">
+      <MenuItem v-for="b in items" :key="b.name" :item="b" />
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return { items: [] };
+  },
+  mounted() {
+    this.$axios.get('items.json').then((response) => {
+      this.items = response.data.burgers;
+    });
+  },
+};
+</script>
