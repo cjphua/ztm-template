d8027863abb0a210c9203e9c31e0ab59612f6c9c
CJ Phua <cjphuaa@gmail.com>
2022-04-22 16:08:48 +0800

    update logo code

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
       <div id="menu" class="grid grid-cols-2 py-5">
         <div id="menu-left">
-          <img src="logo.png" class="w-[150px]" alt="" />
+          <img src="/logo.png" class="w-[150px]" alt="" />
         </div>
         <div id="menu-right">
           <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5">
